cf5610e38c2a7bbd7bf77808bde1f96c04b436ad
guruwanabe <bp.stanciu@gmail.com>
2020-11-14 14:34:06 +0200

    Update rating.ts

--- rating.ts
+++ rating.ts
@@ -17,25 +17,30 @@ class StarRating {
         this.element = element;
         this.options = options;
         this.active = this.element.querySelector<HTMLElement>('.active') ;
-
         this.rating = this.options.initialRating ? this.options.initialRating : 0;
-        this.message = '';
         this.starClass = this.options.starClass ? this.options.starClass : 'star';
-
+        this.message = '';
+        
         if(this.options.messageHelperSelector){
             this.messageHelper = this.element.querySelector<HTMLElement>(this.options.messageHelperSelector);
         }
 
+        if(this.messageHelper){
+            this.message = this.getData(this.messageHelper, 'text');
+        }
+
         this.init();
     }
-
+    /**
+     * Initial setup
+     */
     init():void{
-        this.updateRating();
-        this.saveRating();
-        this.setUpListeners();
         this.setRating(this.rating);
+        this.setUpListeners();
     }
-
+    /**
+     * Set event listeners
+     */
     setUpListeners():void {
         
         const remove = (e:Event) => {
@@ -64,7 +69,9 @@ class StarRating {
             }
         })
     }
-
+    /**
+     * Set the current active element, update the UI
+     */
     setActive(element:HTMLElement):void {
         if (this.active) {
             this.active.classList.remove('active');
@@ -72,48 +79,63 @@ class StarRating {
         element.classList.add('active');
         this.active = element;
         this.setMessage(this.getData(this.active as HTMLElement, 'text'));
-        this.updateRating();
+        this.updateRating(Number(this.getData(this.active, 'value')));
         this.saveRating();
     }
-
-    updateRating():void {
-        if (this.active) {
-            this.rating = Number(this.getData(this.active, 'value'))
-        }
+    /**
+     * Update the rating with the given value.
+     */
+    updateRating(value: number):void {
+        this.rating = value;
     }
-
+    /**
+     * Set the new rating, the corresponding message, save and update the UI
+     */
     setRating(value:number):void {
         if (value > 0) {
             this.element.querySelectorAll(`.${this.starClass}`).forEach((element:Element) => {
-                if (parseInt(this.getData(element as HTMLElement, 'value')!) === value) {
+                if (parseInt(this.getData(element as HTMLElement, 'value')) === value) {
                     this.setActive(element as HTMLElement)
                 }
             });
-            this.rating = value;
-            this.setMessage(this.getData(this.active as HTMLElement, "text"));
-            this.saveRating()
+        }else{
+            this.updateRating(value);
+            this.saveRating();
         }
     }
-
-    saveRating():void {
-        const input = this.element.querySelector<HTMLInputElement>('input');
+    /**
+     * Saves the rating to the given target <HTMLInputElement>
+     * or the first <HTMLInputElement> child of the Element
+     */
+    saveRating(target?: string):void {
+        let input;
+        if(target){
+            input = document.querySelector<HTMLInputElement>(target);
+        }else{
+            input = this.element.querySelector<HTMLInputElement>('input');
+        }
         if(input){
             input.value = this.rating.toString()
         }
     }
-
+    /**
+     * Set the new message and updates the UI
+     */
     setMessage(message?:string):void {
         this.message = message;
         this.updateMessage(this.message);
     }
-
+    /**
+     * Updates the UI with the given message
+     */
     updateMessage(message?:string):void {
-        const helper = this.messageHelper;
-        if(helper){
-            helper.innerHTML = message!
+        if(this.messageHelper){
+            this.messageHelper.innerHTML = message!
         }
     }
-
+    /**
+     * Reset the rating to the given value, updates the message and the UI
+     */
     reset(resetTo:number = 0):void {
         if(this.active){
             this.active.classList.remove("active");
@@ -126,11 +148,15 @@ class StarRating {
         this.active = null;
         this.setRating(resetTo);
     }
-
+    /**
+     * Returns the value of the given element data attribute.
+     */
     getData(element:HTMLElement, name:string): string {
         return element.dataset ? element.dataset[name] as string : element.getAttribute('data-' + name) as string
     }
-
+    /**
+     * Set new value of the given element data attribute.
+     */
     setData(element:HTMLElement, name:string, value:any):void {
         element.dataset ? element.dataset[name] = value : element.setAttribute('data-' + name, value)
     }
